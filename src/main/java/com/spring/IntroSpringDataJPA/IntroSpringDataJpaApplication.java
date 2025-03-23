67392acb50f24af1dacfac613ddc758056c0242b
Edilberto Flores <edilbertof423@gmail.com>
2025-03-23 12:18:54 -0600

    Logro: Creacion de proyecto y creacion de repositorio Git

---
+++ src/main/java/com/spring/IntroSpringDataJPA/IntroSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.IntroSpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/IntroSpringDataJPA/IntroSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.IntroSpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
