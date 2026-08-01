eecb831cc8deccff7f11e309bf8c444a5aa5628a
Edilberto Flores <62002005+EdilbertoFH@users.noreply.github.com>
2026-08-01 15:41:14 -0600

    Logro: Comprender y utilizar query methods para crear consultas mas avanzadas con SpringJPA

--- src/main/java/com/spring/IntroSpringDataJPA/persistence/respository/ICustomerCrudRepository.java
+++ src/main/java/com/spring/IntroSpringDataJPA/persistence/respository/ICustomerCrudRepository.java
@@ -2,8 +2,6 @@ package com.spring.IntroSpringDataJPA.persistence.respository;
 
 import com.spring.IntroSpringDataJPA.persistence.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
